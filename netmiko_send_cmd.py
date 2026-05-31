cefcde09a8a93ba0ad37aba013e15b51869b7236
Ghost70913 <luca.monti230@gmail.com>
2026-05-31 15:31:53 +0200

    Aggiornato netmiko_send_cmd con nuovi parametri

--- netmiko_send_cmd.py
+++ netmiko_send_cmd.py
@@ -1,18 +1,20 @@
 from netmiko import ConnectHandler
 
 dispositivo = {
-    "device_type": "",
-    "host": "",
-    "username": "",
-    "password": "",
-    "port": ,
+    "device_type": "cisco_ios",
+    "host": "10.30.0.24",
+    "username": "admin",
+    "password": "password",
+    "port": 22,
 }
 
 connessione = ConnectHandler(**dispositivo)
 print("Connesso!")
 
-output = connessione.send_command("")
+output = connessione.send_command("ping 8.8.8.8","show ip route") 
+#output1 = connessione.send_command("show ip route")
 print(output)
+#print(output1)
 
 connessione.disconnect()
 print("Disconnesso.")
