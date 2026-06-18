0251a5144e93d25bfcbed50da6ec93925e0c9098
Ghostman <luca.monti230@gmail.com>
2026-06-18 12:27:38 +0200

    v1.0

--- netmiko_send_cmd.py
+++ netmiko_send_cmd.py
@@ -1,20 +1,18 @@
 from netmiko import ConnectHandler
 
 dispositivo = {
-    "device_type": "cisco_ios",
-    "host": "10.30.0.24",
-    "username": "admin",
-    "password": "password",
+    "device_type": "",
+    "host": "",
+    "username": "",
+    "password": "",
     "port": 22,
 }
 
 connessione = ConnectHandler(**dispositivo)
 print("Connesso!")
 
-output = connessione.send_command("ping 8.8.8.8","show ip route") 
-#output1 = connessione.send_command("show ip route")
+output = connessione.send_command("") 
 print(output)
-#print(output1)
 
 connessione.disconnect()
 print("Disconnesso.")
